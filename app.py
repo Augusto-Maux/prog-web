a700f9f4632644f74407cf48b0d19b3f97338541
Augusto-Maux <augusto.maux@escolar.ifrn.edu.br>
2026-03-12 18:37:58 +0000

    Adicionando primeiros códigos

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return 'Olá Mundo!'
+
+@app.route('/contato')
+def contato():
+    return 'augusto.maux@escolar.ifrn.edu.br'

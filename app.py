f7dc7e3fd70d64b837d7e9274a81f694e3e38b45
Augusto-Maux <augusto.maux@escolar.ifrn.edu.br>
2026-03-26 11:39:39 +0000

    Primeiras anotações da aula

--- app.py
+++ app.py
@@ -9,7 +9,29 @@ def index():
 # @app.route('/fale-conosco') -> POR ISSO É MELHOR USAR {{ url_for(função) }}, POIS HÁ DIFERENTES ROTAS PARA MESMA FUNÇÃO
 @app.route('/contato')
 def contato():
-    return render_template('contato.html')
+    # return render_template('contato.html', tel = '(84) 99233-2910', email = 'augusto.maux@escolar.ifrn.edu.br', nome = 'Augusto Maux')
+
+    # nome = "Augusto"
+    # email = "augusto.maux@escolar.ifrn.edu.br"
+    # tel = '(84) 99233-2910'
+    # return render_template('contato.html', nome = nome, email = email, tel = tel)
+
+    dados = { "nome" : "Augusto Maux", "email" : "augusto.maux@escolar.ifrn.edu.br", "tel" : "(84) 99233-2910" }
+    return render_template('contato.html', dados = dados)
+
+@app.route('/usuario', defaults = {"nome" : "Desconhecido", "sobrenome" : "Desconhecido"})
+@app.route('/usuario/<nome>/<sobrenome>')
+def usuario(nome, sobrenome):
+    info = { "nome" : nome, "sobrenome" : sobrenome }
+    return render_template('usuario.html', info = info)
+
+@app.route('/semestre/<int:x>')
+def semestre(x):
+    # data = { "atual" : x, "anterior" : x - 1 }
+    data = {}
+    data["atual"] = x
+    data["anterior"] = x - 1
+    return render_template('semestre.html', data = data)
 
 if __name__ == '__main__':
     app.run(debug=True)

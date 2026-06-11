736606c35fdd961063b8035daa4cb19b98c111f2
Augusto-Maux <augusto.maux@escolar.ifrn.edu.br>
2026-06-11 12:31:00 +0000

    Adicionando atividades de Programação

--- app.py
+++ app.py
@@ -3,10 +3,26 @@ from datetime import datetime # IMPORTAÇÃO
 
 app = Flask(__name__) # INSTÂNCIA DA CLASSE FLASK
 
-@app.route('/<int:qtd>')
-def index(qtd):
-    return render_template('index.html', qtd=qtd)
+@app.route('/')
+def inicial():
+    return render_template('inicial.html')
 
+@app.route('/cardapio')
+def cardapio():
+	return render_template('cardapio.html')
+
+@app.route('/avaliacoes')
+def avaliacao():
+	return render_template('avaliacao.html')
+
+@app.route('/faleconosco')
+def fale_conosco():
+	return render_template('fale_conosco.html')
+
+@app.route('/login')
+def login():
+	return render_template('login.html')
+ 
 
 if __name__ == '__main__':
     app.run(debug=True)
